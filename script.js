6a30c767c7b753f85d6f1b79cb3806c9f914c83c
sophiekalu <sophiekalu.developer@gmail.com>
2020-04-04 20:56:01 +0100

    add toggle done feature

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-var checked = document.getElementById("checked");
-var text = document.getElementById('text');
 
 function inputLength() {
-	return input.value.length;
+  return input.value.length;
 }
 
 function createListElement() {
-	var content = document.createElement("label");
-	content.setAttribute("id", "text");
+  var content = document.createElement("label");
+  content.setAttribute("id", "text");
   content.appendChild(document.createTextNode(input.value));
 
   var checkbox = document.createElement("input");
@@ -19,8 +17,8 @@ function createListElement() {
   var trashIcon = document.createElement("i");
   trashIcon.setAttribute("class", "fa fa-trash icon-trash");
 
-	var item = document.createElement("li");
-	item.setAttribute("class", "list-item");
+  var item = document.createElement("li");
+  item.setAttribute("class", "list-item");
   item.appendChild(checkbox);
   item.appendChild(content);
   item.appendChild(trashIcon);
@@ -31,28 +29,32 @@ function createListElement() {
 }
 
 function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+  if (inputLength() > 0) {
+    createListElement();
+  }
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
 }
 
 function toggleDone(event) {
-  if (text.hasAttribute("class")) {
-    text.removeAttribute("class");
+  var target = event.target;
+  var sibling = target.nextElementSibling;
+
+  if (sibling.hasAttribute("class")) {
+    sibling.removeAttribute("class");
   } else {
-    text.setAttribute("class", "done");
+    sibling.setAttribute("class", "done");
   }
 }
+
 
 
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
 
-checked.addEventListener("click", toggleDone);
+ul.addEventListener("click", toggleDone);
